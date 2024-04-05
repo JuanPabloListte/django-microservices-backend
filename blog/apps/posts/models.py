3d04e0cf549d18810e8bd60fbff5ecde7abd1ac7
Juan Pablo Listte <juanpilistte@gmail.com>
2024-04-05 11:28:02 -0300

    creating app Category

--- blog/apps/posts/models.py
+++ blog/apps/posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=100)
+    picture = models.ImageField(default='media/users/user_default_profile.png',  upload_to='media/users/pictures/', blank=True, null=True, verbose_name='Picture')
+
+    
+    def __str__(self) -> str:
+        return self.username
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.title
